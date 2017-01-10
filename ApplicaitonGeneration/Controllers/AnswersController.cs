3ca022ab63487bea1129f391ddc0cf1f1cca4e0f
Morgan Cooper <mbcooper2@gmail.com>
2017-01-10 00:20:19 -0500

    added question average per question

--- ApplicaitonGeneration/Controllers/AnswersController.cs
+++ ApplicaitonGeneration/Controllers/AnswersController.cs
@@ -30,7 +30,7 @@ namespace ApplicationGeneration.Controllers
             var user = _unitOfWork.Users.Get(User.Identity.GetUserId<int>());
             if (user != null) { CompanyId = user.CompanyId.HasValue ? user.CompanyId.Value : 0; }
 
-            var questions = _unitOfWork.SurveyQuestions.GetAll().Select(q => new { q.Id, q.Question });
+            var questions = _unitOfWork.SurveyQuestions.Find(q => (q.FreeFormComment.HasValue ? !q.FreeFormComment.Value : true)).Select(q => new { q.Id, q.Question });
             IEnumerable<rb_SurveyAnswers> surveyAnswers;
             if (User.IsInRole("SuperAdmin"))
             {
@@ -42,12 +42,16 @@ namespace ApplicationGeneration.Controllers
             }
             foreach (var answer in surveyAnswers)
             {
+                var question = "";
+                var q = questions.FirstOrDefault(que => que.Id == answer.QuestionId);
+                if (q != null)
+                    question = q.Question.Trim();
                 answers.Add(new SurveyViewModel()
                 {
                     id = answer.Id,
                     company = answer.rb_Companies.CompanyName,
                     beacon = answer.BeaconId == null ? 0 : answer.BeaconId.Value,
-                    question = questions.First(q => q.Id == answer.QuestionId).Question.Trim(),
+                    question = question,
                     answer = answer.SurveyAnswer,
                     datesubmitted = answer.DateSubmitted == null ? DateTime.MinValue : answer.DateSubmitted.Value
                 });
@@ -56,33 +60,59 @@ namespace ApplicationGeneration.Controllers
             _unitOfWork.Complete();
 
             // Distribution Answers chart
-            ViewBag.AnswerChart = new List<string[]>();
-            ViewBag.AnswerChart.Add(new string[] { "Answer", "Count" });
-            var res = surveyAnswers  
-                        .Where(sa=> sa.QuestionId != 5)             // Don't include free form comments
-                        .GroupBy(r => r.SurveyAnswer) 
-                        .Select(g => new { Rating = g.Key.Trim(), Count = g.Count() }) 
+            DateTime now = DateTime.Now;
+            ViewBag.AnswerChart = new string[25,5];
+            for (int x=0;x<25; x++)
+            {
+                for (int z = 0; z < 5; z++)
+                {
+                    ViewBag.AnswerChart[x, z] = "0";
+                }
+            }
+            var y = 0;
+            ViewBag.AnswerChart[0, y++] = "Hours";
+            foreach (var question in questions)
+            {
+                ViewBag.AnswerChart[0, y++] = question.Question.Trim();
+            }
+            var res = surveyAnswers
+                        .Where(sa => sa.QuestionId != 5) //&& sa.DateSubmitted > now.AddHours(-24) && sa.DateSubmitted <= now)             // Don't include free form comments]
+                        .GroupBy(
+                        r => new { Hour = (r.DateSubmitted.HasValue ? r.DateSubmitted.Value.Hour : 0), r.SurveyAnswer }, (key, group) => new
+                        {
+                            Key1 = key.Hour,
+                            Key2 = key.SurveyAnswer,
+                            Result = group.ToList()
+                        })
+                        //(r => new { Hour = (r.DateSubmitted.HasValue ? r.DateSubmitted.Value.Hour : 0), Rating = r.SurveyAnswer }) 
+                        //.Select(g => new { GroupedRating = g.Key, Count = g.Count() }) 
                         .ToList();
+
             foreach (var answerCount in res)
             {
-                ViewBag.AnswerChart.Add(new string[] { answerCount.Rating, answerCount.Count.ToString() });
-            }
+                var questionCount = new int[5];
+                var answerTotal = new double[5];
+                var key = answerCount.Key1.ToString();
+                var k = 0;
+                Int32.TryParse(key, out k);
+                foreach (var rating in answerCount.Result)
+                {
+                    var value = 0;
+                    Int32.TryParse(rating.SurveyAnswer, out value);
+                    answerTotal[rating.QuestionId] += value;
+                    questionCount[rating.QuestionId]++;
+                }
 
-            // Distribution questions chart
-            ViewBag.QuestionChart = new List<string[]>();
-            ViewBag.QuestionChart.Add(new string[] { "Question", "Count" });
-            var res2 = surveyAnswers  
-                        .GroupBy(r => r.rb_SurveyQuestions.Id) 
-                        .Select(g => new { QuestionId = g.Key, Count = g.Count() }) 
-                        .ToList();
-            foreach (var answerCount in res2)
-            {
-                ViewBag.QuestionChart.Add(new string[] {
-                    questions
-                        .First(q => q.Id == answerCount.QuestionId)
-                        .Question.Trim(),
-                    answerCount.Count.ToString()
-                });
+                for (int x = 0; x < 5; x++)
+                {
+                    if (questionCount[x] > 0)
+                    {
+                        ViewBag.AnswerChart[k, x] = (answerTotal[x] / questionCount[x]).ToString();
+                    } else
+                    {
+                        ViewBag.AnswerChart[k, x] = "0";
+                    }
+                }
             }
 
             answers.Reverse();
